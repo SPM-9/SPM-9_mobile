502796a491cd2b756001ced170ad81fb9e619de4
嘉然今天吃向晚 <1483823709@qq.com>
2023-12-07 12:06:53 +0800

    教师登录

--- app/src/main/java/com/fxxkywcx/nostudy/network/LoginRegister.java
+++ app/src/main/java/com/fxxkywcx/nostudy/network/LoginRegister.java
@@ -3,6 +3,7 @@ package com.fxxkywcx.nostudy.network;
 import android.os.Handler;
 import android.os.Message;
 import com.fxxkywcx.nostudy.entity.AnnouncementEntity;
+import com.fxxkywcx.nostudy.entity.TeacherEntity;
 import com.fxxkywcx.nostudy.entity.UserEntity;
 import com.fxxkywcx.nostudy.utils.LoginRegisterViews;
 import okhttp3.*;
@@ -14,10 +15,13 @@ public class LoginRegister extends NetworkPackage {
     private final static LoginRegister instance = new LoginRegister();
     private final String urlLogin = servletUrl + "/UserLogin";
     private final String urlRegister = servletUrl + "/UserRegister";
+    private final String urlTeacherLogin = servletUrl + "/TeacherLogin";
     public static final int LOGIN_SUCCESS = 0;
     public static final int LOGIN_FAILED = 1;
     public static final int REGISTER_SUCCESS = 0;
     public static final int DUPLICATE_USERNAME = 1;
+    public static final int USER_LOGIN = 0;
+    public static final int TEACHER_LOGIN = 1;
     private LoginRegister() {}
     public static LoginRegister getInstance() {
         return instance;
@@ -58,6 +62,7 @@ public class LoginRegister extends NetworkPackage {
                         msg.obj = null;
                     } else {
                         msg.arg1 = LOGIN_SUCCESS;
+                        msg.what = USER_LOGIN;
                         UserEntity user = gson.fromJson(respText, UserEntity.class);
                         msg.obj = user;
                     }
@@ -106,6 +111,54 @@ public class LoginRegister extends NetworkPackage {
                         msg.arg1 = REGISTER_SUCCESS;
                     }
                 }
+                handler.sendMessage(msg);
+            }
+        });
+    }
+
+    public void teacherLogin(Handler handler, TeacherEntity teacher) {
+        FormBody body = new FormBody.Builder()
+                .add("userName", teacher.getUserName())
+                .add("password", teacher.getPassword())
+                .build();
+        Call call = okpClient.newCall(new Request.Builder()
+                .url(urlTeacherLogin)
+                .post(body)
+                .build());
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Message msg = Message.obtain();
+                msg.arg2 = NETWORK_FAILURE;
+                msg.obj = null;
+                handler.sendMessage(msg);
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    String message = "Request Unsuccessful " + response.code();
+                    onFailure(call, new IOException(message));
+                    return;
+                }
+
+                Message msg = Message.obtain();
+                msg.arg2 = SUCCEED;
+                if (response.body() != null) {
+                    String respText = response.body().string();
+                    if (respText.equals("false")) {
+                        msg.arg1 = LOGIN_FAILED;
+                        msg.obj = null;
+                    } else {
+                        msg.arg1 = LOGIN_SUCCESS;
+                        msg.what = TEACHER_LOGIN;
+                        TeacherEntity user = gson.fromJson(respText, TeacherEntity.class);
+                        msg.obj = user;
+                    }
+                } else {
+                    msg.obj = null;
+                }
+
                 handler.sendMessage(msg);
             }
         });
